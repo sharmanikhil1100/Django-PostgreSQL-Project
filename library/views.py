a18acfdbd8a795af0a7648e31dbc8c9d4892eba5
Nik <sharmanikhil1100@gmail.com>
2020-10-25 19:10:22 +0530

    Get-  Book and Author. Put- Book.

--- library/views.py
+++ library/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Book, Author
 from .serializers import AuthorSerializer, BookSerializer
 import json
+from django.core.serializers import serialize
 
 def index(request):
     return HttpResponse("Ssup bro")    
@@ -17,33 +18,42 @@ def author(request):
         author_serializer.save()
 
         return HttpResponse(json.dumps({"message":"Author added to Database"}), status=201)
-    
+
     if request.method=="GET":
-        #TODO: Get
-        return HttpResponse("Author Get.")    
+       return HttpResponse(serialize('json',Author.objects.all()))
+
     return HttpResponse("Invalid Request", status=400)
 
 def book(request):
     
-    if request.method=="POST":        
+    if request.method=="POST": 
         input_data=json.loads(request.body)      
         book_serializer = BookSerializer(data=input_data)
         if not book_serializer.is_valid():
             return HttpResponse(json.dumps(book_serializer.errors), status=400)
         book_serializer.save()
-        
         return HttpResponse(json.dumps({"message":"Book added to Database"}), status=201)
     
     if request.method=="PUT":
-        # inputdata[authodid]
-        # x= book.get(authid=ahtbid)
-        # if x==none return httpresp (autho nt foudn)
-        # TODO:
+
+        input_data = json.loads(request.body)        
+        try :
+            book = Book.objects.get(book_id=input_data['book_id'])
+            book.published_date=input_data['published_date']
+            book.name=input_data['name']            
+            # book.author_id=input_data['author_id']
+            book.rating=input_data['rating']                        
+            book.price=input_data['price']
+            book.save()
+            return HttpResponse(json.dumps({"message":"Book updated in Database"}))
+        except Exception as e:
+            print(e)
+            return HttpResponse(json.dumps({'message':'Given book not found in Database'}))           
+
 
     if request.method=="GET":
         """
-        Author GET Request. Returns all the Authors present in the database.
+        Author GET Request. Returns all the Authors present in the Database.
         """
-        #TODO: Get
-        return HttpResponse("Author Get.")
+        return HttpResponse(serialize('json',Book.objects.all()))
     return HttpResponse("Invalid Request", status=400)
